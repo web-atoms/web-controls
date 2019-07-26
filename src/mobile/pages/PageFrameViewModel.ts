ceafb55e8a3e07e0bde90275554a0af64042e9cc
Akash Kava <39438041+ackava@users.noreply.github.com>
2019-07-27 00:15:05 +0530

    wip

--- src/mobile/pages/PageFrameViewModel.ts
+++ src/mobile/pages/PageFrameViewModel.ts
@@ -1,4 +1,5 @@
 import { AtomUri } from "web-atoms-core/dist/core/AtomUri";
+import { CancelToken } from "web-atoms-core/dist/core/types";
 import { Inject } from "web-atoms-core/dist/di/Inject";
 import { NavigationService } from "web-atoms-core/dist/services/NavigationService";
 import { AtomViewModel, Receive } from "web-atoms-core/dist/view-model/AtomViewModel";
@@ -14,6 +15,8 @@ export default class PageFrameViewModel extends AtomViewModel {
     @Inject
     public navigationService: NavigationService;
 
+    public cancelToken: CancelToken;
+
     @Receive("root-page-go-back")
     public async iconClick(): Promise<void> {
         if (this.owner.keepStack && this.owner.stack.length) {
@@ -27,7 +30,13 @@ export default class PageFrameViewModel extends AtomViewModel {
             return;
         }
 
-        await this.navigationService.openPage(this.owner.menuUrl);
+        if (this.cancelToken) {
+            this.cancelToken.cancel();
+            return;
+        }
+
+        this.cancelToken = new CancelToken();
+        await this.navigationService.openPage(this.owner.menuUrl, { cancelToken: this.cancelToken });
     }
 
 }
