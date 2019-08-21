e5e52af254891fb95e6af02b3423e3e363606b9b
Hitesh <hitesh.neurospeech@gmail.com>
2019-08-21 15:53:06 +0530

    Added style

---
+++ src/buttons/AtomPopupButtonStyle.ts
@@ -0,0 +1,21 @@
+import Colors from "web-atoms-core/dist/core/Colors";
+import { AtomStyle } from "web-atoms-core/dist/web/styles/AtomStyle";
+import { IStyleDeclaration } from "web-atoms-core/dist/web/styles/IStyleDeclaration";
+
+export class AtomPopupButtonStyle extends AtomStyle {
+
+    public get root(): IStyleDeclaration {
+        return {
+            subclasses: {
+                ":hover": {
+                    backgroundColor: Colors.gray.withAlphaPercent(0.4)
+                },
+                ".is-open": {
+                    border: "1px solid gray",
+                    backgroundColor: Colors.gray.withAlphaPercent(0.4)
+                }
+            }
+        };
+    }
+
+}
