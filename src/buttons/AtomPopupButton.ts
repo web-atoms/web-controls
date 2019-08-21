40bf252682166c6bd1185d736d4855834a213f4b
Hitesh <hitesh.neurospeech@gmail.com>
2019-08-21 15:31:20 +0530

    Added Popup button

--- src/buttons/AtomPopupButton.ts
+++ src/buttons/AtomPopupButton.ts
@@ -1,4 +1,5 @@
 import { BindableProperty } from "web-atoms-core/dist/core/BindableProperty";
+import { CancelToken } from "web-atoms-core/dist/core/types";
 import { NavigationService } from "web-atoms-core/dist/services/NavigationService";
 import ReferenceService from "web-atoms-core/dist/services/ReferenceService";
 import { AtomControl } from "web-atoms-core/dist/web/controls/AtomControl";
@@ -6,10 +7,15 @@ import { AtomControl } from "web-atoms-core/dist/web/controls/AtomControl";
 export default class AtomPopupButton extends AtomControl {
 
     @BindableProperty
+    public isOpen: boolean = false;
+
+    @BindableProperty
     public popupTemplate: any = null;
 
     @BindableProperty
     public popupParameters: any = null;
+
+    private cancelToken: CancelToken = null;
 
     public preCreate(): void {
         super.preCreate();
@@ -17,26 +23,45 @@ export default class AtomPopupButton extends AtomControl {
         this.bindEvent(
             this.element,
             "click",
-            () => this.openPopup());
+            () => this.app.runAsync(() => this.openPopup()));
     }
 
-    protected openPopup(): Promise<void> {
-        const navigationService = this.app.resolve(NavigationService) as NavigationService;
-        const pt = this.popupTemplate;
-        if (!pt) {
-            // tslint:disable-next-line:no-console
-            console.error("No popup template specified in PopupButton");
+    protected async openPopup(): Promise<void> {
+
+        if (this.cancelToken) {
+            this.cancelToken.cancel();
+            this.cancelToken = null;
             return;
         }
-        let path: string;
-        if (typeof pt === "string") {
-            path = pt;
-        } else {
-            const rs = this.app.resolve(ReferenceService) as ReferenceService;
-            const c = rs.put(pt);
-            path = `app://class/${c.key}`;
+
+        this.cancelToken = new CancelToken();
+
+        try {
+            const navigationService = this.app.resolve(NavigationService) as NavigationService;
+            const pt = this.popupTemplate;
+            if (!pt) {
+                // tslint:disable-next-line:no-console
+                console.error("No popup template specified in PopupButton");
+                return;
+            }
+            let path: string;
+            if (typeof pt === "string") {
+                path = pt;
+            } else {
+                const rs = this.app.resolve(ReferenceService) as ReferenceService;
+                const c = rs.put(pt);
+                path = `app://class/${c.key}`;
+            }
+            this.isOpen = true;
+            const result = await navigationService.openPage(path, this.popupParameters, {
+                cancelToken: this.cancelToken
+            });
+
+            this.element.dispatchEvent(new CustomEvent("result", { detail: result }));
+
+        } finally {
+            this.isOpen = false;
         }
-        return navigationService.openPage(path, this.popupParameters);
     }
 
 }
