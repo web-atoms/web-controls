274918980692238fc8f1d5cd38dac2ee655afe28
Akash Kava <39438041+ackava@users.noreply.github.com>
2020-06-11 16:25:54 +0530

    wip

--- src/switch/AtomSwitch.tsx
+++ src/switch/AtomSwitch.tsx
@@ -1,10 +1,12 @@
 import Bind from "@web-atoms/core/dist/core/Bind";
+import { BindableProperty } from "@web-atoms/core/dist/core/BindableProperty";
 import XNode from "@web-atoms/core/dist/core/XNode";
 import { AtomControl } from "@web-atoms/core/dist/web/controls/AtomControl";
 import AtomSwitchStyle from "./AtomSwitchStyle";
 
 export default class AtomSwitch extends AtomControl {
 
+    @BindableProperty
     public value: boolean;
 
     constructor(app, e) {
@@ -12,7 +14,6 @@ export default class AtomSwitch extends AtomControl {
     }
 
     public create() {
-        this.value = false;
         this.defaultControlStyle = AtomSwitchStyle;
         this.render(<label class={Bind.oneTime(() => this.controlStyle.name + " switch")}>
             <input type="checkbox" checked={Bind.twoWays(() => this.value)}/>
