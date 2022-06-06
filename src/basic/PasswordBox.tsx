725c171e3e380fd65d005ee9ded18b35043cb023
Akash Kava <39438041+ackava@users.noreply.github.com>
2022-06-06 21:56:27 +0530

    Added password box

---
+++ src/basic/PasswordBox.tsx
@@ -0,0 +1,53 @@
+import XNode from "@web-atoms/core/dist/core/XNode";
+import StyleRule from "@web-atoms/core/dist/style/StyleRule";
+import CSS from "@web-atoms/core/dist/web/styles/CSS";
+
+CSS(StyleRule()
+    .display("inline-grid")
+    .gridTemplateColumns("1fr auto")
+    .child(StyleRule("input")
+        .gridRowStart("1")
+        .gridColumnStart("1")
+        .gridColumnEnd("span 2")
+        .and(StyleRule("::-ms-reveal")
+            .display("none")
+        )
+    )
+    .child(StyleRule("i")
+        .gridRowStart("1")
+        .gridColumnStart("2")
+        .zIndex("2")
+    )
+, "div[data-password-box=password-box]");
+
+document.body.addEventListener("togglePassword", (e) => {
+    const current = e.target as HTMLElement;
+    const input = current.previousElementSibling as HTMLInputElement;
+    if (input?.tagName !== "INPUT") {
+        return;
+    }
+    const isPassword = input.type === "password";
+    input.type = isPassword ? "text" : "password";
+    current.className = !isPassword ? "fa-solid fa-eye" : "fa-solid fa-eye-slash";
+    current.title = !isPassword ? "Display Password" : "Hide Password";
+});
+
+export default function PasswordBox({
+    placeholder,
+    value,
+    ... a
+}) {
+    return <div
+        data-password-box="password-box"
+        { ... a}>
+        <input
+            placeholder={placeholder}
+            type="password"
+            value={value} />
+        <i
+            class="fa-solid fa-eye"
+            title="Display Password"
+            data-click-event="togglePassword"
+            data-password-hint="password-hint" />
+    </div>;
+}
