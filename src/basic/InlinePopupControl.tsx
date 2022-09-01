7a53500960878849bbaa844f82b9157ae6eb26f1
Akash Kava <39438041+ackava@users.noreply.github.com>
2022-09-01 14:52:30 +0530

    Popup updated

--- src/basic/InlinePopupControl.tsx
+++ src/basic/InlinePopupControl.tsx
@@ -3,42 +3,13 @@ import Colors from "@web-atoms/core/dist/core/Colors";
 import XNode from "@web-atoms/core/dist/core/XNode";
 import StyleRule from "@web-atoms/core/dist/style/StyleRule";
 import { AtomControl, ElementValueSetters } from "@web-atoms/core/dist/web/controls/AtomControl";
-import { IPopupOptions } from "@web-atoms/core/dist/web/services/PopupService";
-import CSS from "@web-atoms/core/dist/web/styles/CSS";
-
-function closeHandler(
-    host: HTMLElement,
-    opener: HTMLElement,
-    container, close) {
-    let handler: any = null;
-    handler = (e: Event) => {
-        let start = e.target as HTMLElement;
-        if (e.defaultPrevented) {
-            return;
-        }
-        while (start) {
-            if (start === host) {
-                break;
-            }
-            if (start === opener) {
-                return;
-            }
-            if (start === container.element) {
-                return;
-            }
-            start = start.parentElement;
-        }
-        close();
-    };
-    document.body.addEventListener("click", handler);
-    container.registerDisposable(() => document.body.removeEventListener("click", handler));
-}
+import { IPopupOptions, PopupControl } from "@web-atoms/core/dist/web/services/PopupService";
 
 export interface IInlinePopupOptions extends IPopupOptions {
     cancelOnClick?: boolean;
 }
 
-export default class InlinePopupControl extends AtomControl {
+export default class InlinePopupControl extends PopupControl {
 
     public static showPopup<T>(opener: HTMLElement | AtomControl, popup: XNode, options: IInlinePopupOptions = {}) {
         const c = class extends InlinePopupControl {
@@ -63,76 +34,6 @@ export default class InlinePopupControl extends AtomControl {
             }
         };
         return c.showControl(opener, options);
-    }
-
-    public static showControl<T>(
-        opener: HTMLElement | AtomControl,
-        options: IPopupOptions = {}
-    ) {
-        return new Promise<T>((resolve, reject) => {
-            let openerElement: HTMLElement = options?.parentElement;
-            let app: App;
-            if (opener instanceof AtomControl) {
-                openerElement ??= opener.element;
-                app = opener.app;
-            } else {
-                openerElement ??= opener;
-                let start = opener;
-                while (!start.atomControl) {
-                    start = start.parentElement;
-                }
-                if (!start) {
-                    return Promise.reject("Could not create popup as target is not attached");
-                }
-                app = start.atomControl.app;
-            }
-            const popup = new this(app);
-            const container = document.createElement("div");
-            container.dataset.inlinePopup = "true";
-
-            container.appendChild(popup.element);
-            if (openerElement.offsetParent !== openerElement.parentElement) {
-                openerElement.parentElement.style.position = "relative";
-            }
-            setTimeout(() => {
-                container.style.top = (openerElement.offsetTop + openerElement.offsetHeight) + "px";
-                openerElement.insertAdjacentElement("afterend", container);
-            }, 50);
-
-            let resolved = false;
-            const { cancelToken } = options;
-            if (cancelToken) {
-                cancelToken.registerForCancel((r) => {
-                    popup.cancel(r);
-                });
-            }
-            popup.close = (r) => {
-                if (resolved) {
-                    return;
-                }
-                resolved = true;
-                resolve(r);
-                popup.dispose();
-                container.remove();
-            };
-            popup.cancel = (r = "cancelled") => {
-                if (resolved) {
-                    return;
-                }
-                resolved = true;
-                reject(r);
-                popup.dispose();
-                container.remove();
-            };
-
-            closeHandler(openerElement.parentElement, openerElement, popup, popup.cancel);
-        });
-    }
-
-    public close(r?) {}
-
-    public cancel(r?) {
-
     }
 
 }
