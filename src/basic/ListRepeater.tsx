25f37357067de03e7f12b5ec1148b69090624d79
Akash Kava <39438041+ackava@users.noreply.github.com>
2022-10-13 15:22:47 +0530

    wip

--- src/basic/ListRepeater.tsx
+++ src/basic/ListRepeater.tsx
@@ -4,7 +4,15 @@ import CSS from "@web-atoms/core/dist/web/styles/CSS";
 import AtomRepeater from "./AtomRepeater";
 
 CSS(StyleRule()
-    .child(StyleRule("[data-item-index][data-selected-item=true]")
+    .child(StyleRule("*[data-item-index]")
+        .backgroundColor("var(--list-bg-color)")
+        .color("var(--list-color)")
+        .hover(StyleRule()
+            .backgroundColor("var(--list-selected-bg-color-hover, lightgreen)")
+            .color("var(--list-selected-color-hover, inherit)")
+        )
+    )
+    .child(StyleRule("*[data-item-index][data-selected-item=true]")
         .backgroundColor("var(--list-selected-bg-color, lightgray)")
         .color("var(--list-selected-color, inherit)")
         .hover(StyleRule()
