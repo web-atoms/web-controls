6b0de1608a14fcf5365d828d3181d60607135a9d
Akash Kava <39438041+ackava@users.noreply.github.com>
2022-12-02 14:42:18 +0530

    Made parameters optional

--- src/PageNavigator.ts
+++ src/PageNavigator.ts
@@ -3,7 +3,7 @@ import type { ContentPage } from "./mobile-app/MobileApp";
 
 export default class PageNavigator {
 
-    public static openPage<T>(page: typeof ContentPage<T>, parameters: T, clearHistory?: boolean): void;
+    public static openPage<T>(page: typeof ContentPage<T>, parameters?: T, clearHistory?: boolean): void;
     public static openPage(
         page: string | any,
         parameters: {[key: string]: any} = {},
@@ -16,7 +16,7 @@ export default class PageNavigator {
         });
     }
 
-    public static pushPage<T>(page: typeof ContentPage<T>, parameters: T, clearHistory?: boolean);
+    public static pushPage<T>(page: typeof ContentPage<T>, parameters?: T, clearHistory?: boolean);
     public static pushPage(
         page: string | any,
         parameters: {[key: string]: any} = {},
@@ -30,7 +30,7 @@ export default class PageNavigator {
     }
 
     public static pushPageForResult<T, TResult>(
-        page: typeof ContentPage<T, TResult>, parameters: T, clearHistory?: boolean): Promise<TResult>;
+        page: typeof ContentPage<T, TResult>, parameters?: T, clearHistory?: boolean): Promise<TResult>;
     public static async pushPageForResult<T>(
         page: string | any,
         parameters: {[key: string]: any} = {},
