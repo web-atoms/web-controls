6fa7ee18acb91d613c17ab616e02ce09829fc038
Akash Kava <39438041+ackava@users.noreply.github.com>
2023-03-03 21:24:56 +0530

    Added parent for childSelector

--- src/basic/MergeNode.tsx
+++ src/basic/MergeNode.tsx
@@ -8,14 +8,20 @@ export default class MergeNode {
         return new MergeNode(empty);
     }
 
-    public static childSelector(name: string) {
+    public static childSelector(name: string, parent?: string) {
+        if (parent !== void 0) {
+            return new MergeNode([{ replace: "* > " + name, parent }]);
+        }
         return new MergeNode(["* > " + name]);
     }
 
     private constructor(public classes: IReplaceClass[] = empty) {
     }
 
-    public childSelector(name: string) {
+    public childSelector(name: string, parent?: string) {
+        if (parent !== void 0) {
+            return new MergeNode([ ... this.classes, { replace: "* > " + name, parent }]);
+        }
         return new MergeNode([... this.classes, "* > " + name]);
     }
 }
