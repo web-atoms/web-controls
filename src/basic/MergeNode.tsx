aff3d4546ae0c652f70dab7c2811a898ff8c592c
Akash Kava <39438041+ackava@users.noreply.github.com>
2023-03-03 22:09:44 +0530

    Added nested

--- src/basic/MergeNode.tsx
+++ src/basic/MergeNode.tsx
@@ -20,6 +20,14 @@ export default class MergeNode {
         return new MergeNode(["* > " + name]);
     }
 
+    public static nestedSelector(name: string, p?: { after?: string, parent?: string }) {
+        if (p !== void 0) {
+            const { parent, after } = p;
+            return new MergeNode([{ replace: "* " + name, parent, after }]);
+        }
+        return new MergeNode(["* " + name]);
+    }
+
     private constructor(public classes: IReplaceClass[] = empty) {
     }
 
@@ -30,4 +38,13 @@ export default class MergeNode {
         }
         return new MergeNode([... this.classes, "* > " + name]);
     }
+
+    public nestedSelector(name: string, p?: { after?: string, parent?: string }) {
+        if (p !== void 0) {
+            const { parent, after } = p;
+            return new MergeNode([ ... this.classes, { replace: "* " + name, parent, after }]);
+        }
+        return new MergeNode([... this.classes, "* " + name]);
+    }
+
 }
