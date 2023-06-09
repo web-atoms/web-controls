53cad696f630df5d9649200a5d7d0bcf567c6a74
Akash Kava <39438041+ackava@users.noreply.github.com>
2023-06-09 18:10:08 +0530

    Added re exports

--- src/PageNavigator.ts
+++ src/PageNavigator.ts
@@ -1,5 +1,5 @@
 import { CancelToken } from "@web-atoms/core/dist/core/types";
-import type { ContentPage } from "./mobile-app/MobileApp";
+import { ContentPage } from "./mobile-app/ContentPage";
 
 export type Page<T> = typeof ContentPage<T, any>;
 export type PageWith<T, T2> = typeof ContentPage<T, T2>;
--- src/mobile-app/MasterDetailPage.tsx
+++ src/mobile-app/MasterDetailPage.tsx
@@ -2,8 +2,9 @@ import XNode from "@web-atoms/core/dist/core/XNode";
 import styled from "@web-atoms/core/dist/style/styled";
 import { AtomControl } from "@web-atoms/core/dist/web/controls/AtomControl";
 import PageNavigator, { Page } from "../PageNavigator";
-import { ContentPage, isMobileView } from "./MobileApp";
+import { isMobileView } from "./MobileApp";
 import { IDisposable } from "@web-atoms/core/dist/core/types";
+import { ContentPage } from "./ContentPage";
 
     styled.css `
         & > [data-page-element=content] {
