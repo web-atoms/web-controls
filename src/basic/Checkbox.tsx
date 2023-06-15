27058784159435efa9ba62dad263efb729cf620e
Akash Kava <39438041+ackava@users.noreply.github.com>
2023-06-15 10:10:42 +0530

    added extra

--- src/basic/Checkbox.tsx
+++ src/basic/Checkbox.tsx
@@ -1,15 +1,19 @@
 import XNode from "@web-atoms/core/dist/core/XNode";
 
 import "./styles/checkbox-style";
+import IElement from "./IElement";
+
+export interface ICheckbox extends IElement {
+    checked: any;
+    text: string;
+}
 
 export default function Checkbox({
     checked,
-    text
-}: {
-    checked: any,
-    text: string
-}) {
-    return <div data-check-box="check-box">
+    text,
+    ... a
+}: ICheckbox) {
+    return <div data-check-box="check-box" { ... a}>
         <label>
             <input
                 type="checkbox"
