61056a58df264429ea711818371c5075b2f488c2
Akash Kava <39438041+ackava@users.noreply.github.com>
2024-07-12 21:55:57 +0530

    Auto focus moved

--- src/basic/PasswordBox.tsx
+++ src/basic/PasswordBox.tsx
@@ -23,6 +23,7 @@ export interface IPasswordBox extends IElement {
 export default function PasswordBox({
     placeholder,
     value,
+    autofocus,
     ... a
 }: IPasswordBox) {
     return <div
@@ -30,6 +31,7 @@ export default function PasswordBox({
         { ... a}>
         <input
             placeholder={placeholder}
+            autofocus={autofocus}
             type="password"
             value={value} />
         <i
