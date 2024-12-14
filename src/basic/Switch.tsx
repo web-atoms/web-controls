dc59e3e99693b2ff9ce1ae04103629bc8b9edaa5
Akash Kava <39438041+ackava@users.noreply.github.com>
2024-12-14 19:34:40 +0530

    Style replaced

--- src/basic/Switch.tsx
+++ src/basic/Switch.tsx
@@ -1,7 +1,7 @@
 import XNode from "@web-atoms/core/dist/core/XNode";
 import IElement from "./IElement";
 
-import "./styles/switch-style";
+import "./styles/switch.global.less";
 
 /**
  * Original source = https://www.htmllion.com/css3-toggle-switch-button.html
