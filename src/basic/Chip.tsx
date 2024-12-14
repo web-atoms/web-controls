0f8febe996a2883b6c91cf8722625d5e2b0d63ac
Akash Kava <39438041+ackava@users.noreply.github.com>
2024-12-14 18:37:37 +0530

    Added chip.less

--- src/basic/Chip.tsx
+++ src/basic/Chip.tsx
@@ -1,7 +1,7 @@
 import XNode from "@web-atoms/core/dist/core/XNode";
 import IElement from "./IElement";
 
-import "./styles/chip-style";
+import "./styles/chip.less";
 
 export interface IChip extends IElement {
     icon?: string;
