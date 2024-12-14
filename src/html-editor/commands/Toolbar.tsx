55b04cbcd86d71eed0ee98cdbefce9282bf7dc36
Akash Kava <39438041+ackava@users.noreply.github.com>
2024-12-14 21:19:18 +0530

    Style moved

--- src/html-editor/commands/Toolbar.tsx
+++ src/html-editor/commands/Toolbar.tsx
@@ -1,23 +1,6 @@
 import XNode from "@web-atoms/core/dist/core/XNode";
-import styled from "@web-atoms/core/dist/style/styled";
 
-    styled.css `
-        display: flex;
-        flex-wrap: wrap;
-        align-items: center;
-        align-content: center;
-        justify-content: center;
-        gap: var(--spacing, 5px);
-
-        & .command {
-            display: flex;
-            align-content: center;
-            justify-content: center;
-            align-items: center;
-            justify-items: center;
-        }
-
-    `.installGlobal("[data-html-editor-element=toolbar]")
+import "../../styles/html-editor-toolbar.global.less";
 
 export default function Toolbar(a: any, ... nodes: XNode[]) {
     return <div data-html-editor-element="toolbar" { ... a}>
