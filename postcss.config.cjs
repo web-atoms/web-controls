a3208f34595f8c7cc4cce1e39b0f6d50205d4916
Akash Kava <39438041+ackava@users.noreply.github.com>
2025-04-04 19:42:20 +0530

    vb

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -2,9 +2,11 @@ module.exports = (ctx) => ({
     map: { ... ctx.options.map, sourcesContent: false },
     plugins: [
         require("postcss-import-styled-js")(),
-        require('postcss-preset-env')(),
-        require("postcss-import")(),
+        require('postcss-preset-env')({
+          stage: 4
+        }),
         require("postcss-import-ext-glob")(),
+        require("postcss-import")(),
         require("postcss-nested")(),
         require("postcss-copy-assets")({ base: ctx.options.base }),
         require("cssnano")()
