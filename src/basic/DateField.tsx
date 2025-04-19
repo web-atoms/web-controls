63a8c22e5fa25f98145b44a9e0d1a453df60b4f8
Akash Kava <39438041+ackava@users.noreply.github.com>
2025-04-19 15:31:00 +0530

    default anchor changed

--- src/basic/DateField.tsx
+++ src/basic/DateField.tsx
@@ -182,7 +182,7 @@ export default class DateField extends AtomControl {
             data-layout="flex"
             data-date-field="date-field"
             text={Bind.oneWay(() => this.format?.(this.value, this.enableTime) || this.prompt)}
-            event-click={() => CalendarPopup.show(this)}>
+            event-click={() => CalendarPopup.show(this, { "anchor-left": "parent-left"})}>
         </div>);
     }
 
