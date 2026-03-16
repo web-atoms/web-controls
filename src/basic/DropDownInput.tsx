e255a0c97e9e6e98e2cc5c328b0441a8545e689e
Akash Kava <39438041+ackava@users.noreply.github.com>
2026-03-17 01:51:35 +0530

    wip

--- src/basic/DropDownInput.tsx
+++ src/basic/DropDownInput.tsx
@@ -138,7 +138,7 @@ public "event-selection-changed"?: (e: CustomEvent) => void;
                     }
                 ));
             }
-            Focusable.moveNext(this.input);
+            // Focusable.moveNext(this.input);
         } finally {
             this.isPopupOpen = false;
         } 
